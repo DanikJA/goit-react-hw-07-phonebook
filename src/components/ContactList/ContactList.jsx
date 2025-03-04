e1ee5007d3fddb85e0903c6fcf0849b38c3200f1
Danik2023 <danik.kyrteu2@gmail.com>
2025-03-04 19:57:00 +0200

    ...

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -10,12 +10,12 @@ import { selectFilter, selectContacts } from '../../redux/selectors.js';
 export const ContactListComponent = () => {
   const contacts = useSelector(selectContacts);
 
-  const filterValue = useSelector(selectFilter);
+  const filter = useSelector(selectFilter);
 
   const filteredContacts = contacts.filter(
     contact =>
-      contact.name.toLowerCase().includes(filterValue.toLowerCase()) ||
-      contact.number.includes(filterValue)
+      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+      contact.number.includes(filter)
   );
 
   return (
