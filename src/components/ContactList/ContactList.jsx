aeb33b4fc1dc1f3109a2ed9bee1804c89cbd76cf
Danik2023 <danik.kyrteu2@gmail.com>
2025-03-05 10:13:12 +0200

    changed filter

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -14,7 +14,7 @@ import {
 
 export const ContactListComponent = () => {
   const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
+  const filterValue = useSelector(selectFilter);
   const isLoadingContacts = useSelector(isLoading);
 
   if (!isLoadingContacts) {
@@ -33,8 +33,8 @@ export const ContactListComponent = () => {
   }
   const filteredContacts = contacts.filter(
     contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
-      contact.number.includes(filter)
+      contact.name.toLowerCase().includes(filterValue.toLowerCase()) ||
+      contact.number.includes(filterValue)
   );
 
   return (
